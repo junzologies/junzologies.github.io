3b9efd4bfb0d1b30333178a51cfeefdf00260cf3
Wong Jun Ming <1176998+junzologies@users.noreply.github.com>
2020-05-25 17:48:39 +0800

    add panoanatomy

---
+++ apps/panoanatomy/precache-manifest.519b11a66b0bb3aa52d5c2d1a6e05e1d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a90282f4487509a933c5d5e83a753f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bde513e0688890b90e9",
+    "url": "./static/css/2.727a04f1.chunk.css"
+  },
+  {
+    "revision": "d5092f8fdd8e5056e0a0",
+    "url": "./static/css/main.80c06d0c.chunk.css"
+  },
+  {
+    "revision": "7bde513e0688890b90e9",
+    "url": "./static/js/2.5611dd45.chunk.js"
+  },
+  {
+    "revision": "7ea3f0454d578d08a1d13747d5dd3f95",
+    "url": "./static/js/2.5611dd45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5092f8fdd8e5056e0a0",
+    "url": "./static/js/main.76630dd7.chunk.js"
+  },
+  {
+    "revision": "0734a686b58c126b6b5d",
+    "url": "./static/js/runtime-main.7614c5c4.js"
+  },
+  {
+    "revision": "0e623c489f353a7defa45390a69518d3",
+    "url": "./static/media/case1.0e623c48.jpg"
+  },
+  {
+    "revision": "dda98717be7ca25795cd78fe558ecafd",
+    "url": "./static/media/case2.dda98717.jpg"
+  },
+  {
+    "revision": "ddd222a6e10e81b46ae4bf1d8fff7882",
+    "url": "./static/media/case3.ddd222a6.jpg"
+  },
+  {
+    "revision": "f24594815dc9e921ff2d7a5da60d0e19",
+    "url": "./static/media/case4.f2459481.jpg"
+  },
+  {
+    "revision": "adf8ceef1d0ac0deadc2da1c697dff6a",
+    "url": "./static/media/case5.adf8ceef.jpg"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "./static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "./static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "./static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "./static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "./static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "./static/media/icons-20.ca7934b3.eot"
+  },
+  {
+    "revision": "a4f2a77e48311949bbe0818e6d5d6986",
+    "url": "./static/media/nus-blue-logo.a4f2a77e.png"
+  }
+]);
